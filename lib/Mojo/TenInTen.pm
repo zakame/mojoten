a1d2563b7a73ef247703f528fea362649fc3c562
Zak B. Elep <zakame@zakame.net>
2011-08-21 21:32:25 +0800

    Prevent caching of JSON responses
    Found an issue where /randomquote/quote will not be requested again by
    IE upon executing jQuery.getJSON().  A bit of reading in
    http://stackoverflow.com/questions/264216/getjson-returning-cached-data-in-ie8
    offers the solution of either configuring jQuery.ajaxSetup() to not
    cache, or let the server/app respond with the appropriate
    Cache-Control and Expires headers.
    
    See also http://toroid.org/ams/etc/mojolicious-static-resources and
    http://www.mnot.net/cache_docs/ , where the server-side solution came
    from.

--- lib/Mojo/TenInTen.pm
+++ lib/Mojo/TenInTen.pm
@@ -18,6 +18,20 @@ sub startup {
   $r->route('/randomquote')->to('random_quote#index');
   $r->route('/randomquote/quote')->to('random_quote#quote');
   $r->route('/randomquote/show_all')->to('random_quote#show_all');
+
+  # prevent caching for JSON responses for IE (taken from
+  # http://toroid.org/ams/etc/mojolicious-static-resources)
+  $self->hook(after_dispatch => sub {
+    my $tx   = shift;
+    my $type = $tx->res->headers->content_type;
+    return unless $type =~ /json/;
+    $tx->res->headers->header(
+      Expires => Mojo::Date->new(time-365*86400)
+    );
+    $tx->res->headers->header(
+      'Cache-Control' => 'max-age=1, no-cache'
+    );
+  });
 }
 
 1;
