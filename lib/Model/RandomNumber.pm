11a35b741b1a51bf3f751ee7771652b203785e3d
Zak B. Elep <zakame@zakame.net>
2018-03-31 23:43:24 +0800

    Use Mojo::Base for Model::RandomNumber
    Simplify things a bit now for later...

--- lib/Model/RandomNumber.pm
+++ lib/Model/RandomNumber.pm
@@ -1,13 +1,9 @@
 package Model::RandomNumber;
-use strict;
-use warnings;
+use Mojo::Base -base;
 use Carp;
 
-sub new {
-  my $self = shift;
-  my %args = @_;
-  bless {lower => $args{lower} || 0, upper => $args{upper} || 1} => $self;
-}
+has 'lower' => 0;
+has 'upper' => 1;
 
 sub generate {
   my ($self, %args) = @_;
@@ -18,12 +14,12 @@ sub generate {
     if keys %args;
 
   croak 'upper bound not greater than lower bound'
-    unless $self->{upper} > $self->{lower};
+    unless $self->upper > $self->lower;
 
-  my $range = $self->{upper} - $self->{lower};
+  my $range = $self->upper - $self->lower;
   $integer
-    ? int(rand($range) + $self->{lower} + 0.5 * ($range <=> 0))
-    : rand($range) + $self->{lower};
+    ? int(rand($range) + $self->lower + 0.5 * ($range <=> 0))
+    : rand($range) + $self->lower;
 }
 
 1;
