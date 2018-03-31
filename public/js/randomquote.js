054432883ab0b2ef4ba92e353f09986e4ebfb552
Zak B. Elep <zakame@zakame.net>
2018-03-31 11:21:41 +0800

    :boxing-glove: KO randomquote
    This is fairly straightforward.  Removed the jQuery animations though,
    since that would mean adding a `bindingHandler` to KO.

--- public/js/randomquote.js
+++ public/js/randomquote.js
@@ -1,19 +1,23 @@
 requirejs.config({ paths: { jquery: '/mojo/jquery/jquery' } });
 
-requirejs(['jquery', 'domReady!'], function ($) {
-  $('#getquote').click(function () {
-    $.getJSON("/randomquote/quote", function (data) {
-      $('blockquote').empty().hide().append(data).show('slow');
-    });
-  });
-  $('#getallquotes').click(function () {
-    $.getJSON("/randomquote/show_all", function (data) {
-      var items = [];
-      $('#allquotes').empty();
-      $.each(data, function () {
-        items.push('<li>' + this + '</li>');
-      });
-      $('#allquotes').append(items.join(''));
-    });
-  });
+require(['jquery', 'knockout', 'domReady!'], function ($, ko) {
+  'use strict';
+
+  var RandomQuotesViewModel = function () {
+    var self = this;
+
+    self.quote = ko.observable('');
+    self.quotes = ko.observableArray([]);
+
+    self.getQuote = function () {
+      $.getJSON('/randomquote/quote')
+        .done(function (data) { self.quote(data) });
+    };
+    self.getAllQuotes = function () {
+      $.getJSON('/randomquote/show_all')
+        .done(function (data) { self.quotes(data) });
+    };
+  };
+
+  ko.applyBindings(new RandomQuotesViewModel());
 });
