e89de713a155e4a0fd031f69b0c1cda49adc59d7
Zak B. Elep <zakame@zakame.net>
2018-04-05 21:01:35 +0800

    :lipstick: factor out $.ajax calls for random numbers
    In case of replacing jQuery later with something else...

--- public/js/randomnumber.js
+++ public/js/randomnumber.js
@@ -3,20 +3,25 @@ requirejs.config({ paths: { jquery: '/mojo/jquery/jquery' } });
 require(['jquery', 'knockout', 'domReady!'], function ($, ko) {
   'use strict';
 
+  var get = function (route, observable) {
+    $.ajax(route).done(function (data) {
+      observable(data);
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+      observable(errorThrown + ' :p');
+    });
+  };
+
   var RandomNumberViewModel = function () {
     var self = this;
 
     self.randomNumber = ko.observable();
     self.rollResult = ko.observable();
 
-    $.ajax('/randomnumber/get').done(function (data) {
-      self.randomNumber(data);
-    });
+    get('/randomnumber/get', self.randomNumber);
 
     self.rollD6 = function () {
-      $.ajax('/randomnumber/d6').done(function (data) {
-        self.rollResult(data);
-      });
+      self.rollResult('rolling...');
+      get('/randomnumber/d6', self.rollResult);
     };
   };
 
